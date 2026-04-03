ac38bf7d9696ab5096d4407ea13da7eae1937f36
prakashhg <124186725+prakashhg@users.noreply.github.com>
2026-04-03 12:16:54 +0530

    Create outputs.tf

---
+++ .github/workflows/outputs.tf
@@ -0,0 +1,11 @@
+output "foundry_account_id" {
+  value = module.foundry.ai_foundry_account_id
+}
+
+output "foundry_project_id" {
+  value = module.foundry.project_id
+}
+
+output "agent_service_endpoint" {
+  value = module.foundry.agent_service_endpoint
+}
